ef5192468843b665c0dd0d13d16e4ae3b5b19059
Magali Garot <magali.garot@gmail.com>
2021-03-01 16:42:25 +0100

    apipokemon

---
+++ pokedex.js
@@ -0,0 +1,93 @@
+// DOCUMENTATION :
+//https://www.youtube.com/watch?v=XL68br6JyYs&t=845s
+//https://www.youtube.com/watch?v=4Qyo6veprVY&t=1166s
+//GIF PIKACHU : https://www.planete-smiley.com/emoticones/Pokemon/57-3.php
+//Modal : https://medium.com/@nerdplusdog/a-how-to-guide-for-modal-boxes-with-javascript-html-and-css-6a49d063987e
+
+const poke_container = document.getElementById('poke_container');
+
+//Permet de récupérer les 151 du fichiers
+const fetchPokemons = async () => {
+  for(let i=1; i<=151; i++){
+    await getPokemon(i);
+  }
+}
+////////////////////////////////////////
+
+const colors = {
+	fire: '#d80100',
+	grass: '#7ccb63',
+	electric: '#f7ee00',
+	water: '#5da2f7',
+	ground: '#89370b',
+	rock: '#3c1c0a',
+	fairy: '#e9aaa4',
+	poison: '#ab80b7',
+	bug: '#037195',
+	dragon: '#f2aa0f',
+	psychic: '#8800f7',
+	flying: '#838582',
+	fighting: '#838582',
+	normal: '#a2abb3'
+};
+const main_types = Object.keys(colors);
+
+
+///////////////////////////////////////
+
+/*URL API*/
+const getPokemon = async id => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+  const res = await fetch(url);
+  const pokemon = await res.json();
+  createPokemonCard(pokemon);
+}
+
+//Appel les ID via la boucle for
+fetchPokemons();
+
+//Crée les cartes
+function createPokemonCard(pokemon){
+const pokemonEl = document.createElement('div');
+pokemonEl.classList.add('pokemon');
+
+//Appel le type du pokemon
+const poke_types = pokemon.types.map(el => el.type.name);
+// color type
+const type = main_types.find(type => poke_types.indexOf(type) > -1);
+const color = colors[type];
+pokemonEl.style.backgroundColor = color;
+
+//Permet de mettre une Maj
+const name = pokemon.name[0].toUpperCase() + pokemon.name.slice(1);
+
+//Autre façon de faire :
+// const pokeInnerHTML = `${pokemon.id}` + `${pokemon.name}`;
+const pokeInnerHTML = 
+  `
+            <div class="infos">
+              <span class="infos__number"># ${pokemon.id}</span> 
+              <span class="infos__title">${name}</span> 
+            </div>
+
+            <div class="spriteOrigine">
+              <img class="spriteOrigine__picture" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${name}"/>
+            </div>
+
+            <p class="infos__types">${poke_types}</p> 
+
+            <div class="spriteGame">  
+              <img class="spriteGame__Face" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png" alt="${name}"/>
+              <img class="spriteGame__Back"  src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.id}.png" alt="${name}"/>
+            </div>
+    
+  `;
+pokemonEl.innerHTML = pokeInnerHTML;
+poke_container.appendChild(pokemonEl);
+
+}
+
+//SPRITE CARTOON
+{/* <div class="spriteCartoon">
+  <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg" alt="${name}"/>
+</div> */}
---
+++ searchpokemon.js
@@ -0,0 +1,37 @@
+let cardImgOne = document.querySelector(".card-img-One");
+let cardImgTwo = document.querySelector(".card-img-Two");
+let cardPicture = document.querySelector(".card-picture");
+let name = document.querySelector(".card-name");
+let type = document.querySelector(".card-type");
+let generateBtn = document.querySelector(".submit-button");
+
+generateBtn.addEventListener("click", e => {
+    e.preventDefault();
+
+    let id = document.querySelector(".search-input").value;
+
+    async function getTwoPokemon() {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`);
+        const data = await res.json();
+
+        console.log(data);
+
+        let imgOne = data.sprites.front_default;
+        let imgTwo = data.sprites.back_default;
+        let picture = data.sprites.other.dream_world.front_default;
+        let newName = data.name;
+        let newTypes = data.types[0].type.name;
+
+        newName = newName.charAt(0).toUpperCase() + newName.slice(1);
+        name.innerText = newName;
+
+        newTypes = newTypes.charAt(0).toUpperCase() + newTypes.slice(1);
+        type.innerText = newTypes;
+
+        cardImgOne.innerHTML = `<img src=${imgOne}>`;
+        cardImgTwo.innerHTML = `<img src=${imgTwo}>`;
+        cardPicture.innerHTML = `<img src=${picture}>`;
+    }
+
+    getTwoPokemon();
+});
